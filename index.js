f379e85fc1b49a71420849c22446430335e86edc
joy Kurui <kurui.chepngetich@gmail.com>
2023-10-05 22:22:32 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function saturdayFun(defaultActivity = "roller-skate"){
+   return `This Saturday, I want to ${defaultActivity}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+
+function mondayWork(defaultParam = "go to the office"){
+    return `This Monday, I will ${defaultParam}.`;    
+}
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective(flair = "*") {
+    return function innerFunc(defaultInner = "special") {
+        return `You are ${flair}${defaultInner}${flair}!`;
+    }
+}
+
+wrapAdjective("a hard worker");
+wrapAdjective("||")("a dedicated programmer");
+
+
+
+
+
